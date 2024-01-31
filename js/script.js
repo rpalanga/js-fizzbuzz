491258a8dbc029a2a942dca052b33fda9d3c34ff
roberto-palanga <r.palanga94@gmail.com>
2024-01-31 17:34:50 +0100

    Fine MileStone 1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 const grigliaElement = document.querySelector("#griglia")
 
-for (let i = 0 ; i <= 100 ; i++) {
+for (let i = 1 ; i <= 100 ; i++) {
     
-    
-    grigliaElement.innerHTML += `<div>salve mondo</div>`
-    console.log(grigliaElement)
+    const newElement = document.createElement ("div");
+    newElement.className = "CassOne"
+    newElement.innerHTML = i ;
+    grigliaElement.append(newElement);
+
+    if (i % 3 == 0){
+        newElement.innerText =
+        "Fizz";
+        
+        } else if (i % 5 == 0){
+            newElement.innerText =
+            "Buzz";
+        }
+    if (i % 15 == 0){
+        newElement.innerText =
+            "FizzBuzz";
+    }
+        
+    console.log(i)
+   
 }
