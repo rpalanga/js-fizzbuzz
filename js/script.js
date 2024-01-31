36acc5492e234d60f25b312d423dc49f31191885
roberto-palanga <r.palanga94@gmail.com>
2024-01-31 18:23:03 +0100

    Esercizio finito

--- js/script.js
+++ js/script.js
@@ -6,18 +6,26 @@ for (let i = 1 ; i <= 100 ; i++) {
     newElement.className = "CassOne"
     newElement.innerHTML = i ;
     grigliaElement.append(newElement);
+    newElement.style.backgroundColor = "#3F87AE";
+
 
     if (i % 3 == 0){
         newElement.innerText =
         "Fizz";
+        newElement.style.backgroundColor = "#62D3A4";
         
         } else if (i % 5 == 0){
             newElement.innerText =
             "Buzz";
+        newElement.style.backgroundColor = "#F8D377";
+
+            
         }
     if (i % 15 == 0){
         newElement.innerText =
             "FizzBuzz";
+        newElement.style.backgroundColor = "#DE5471";
+
     }
         
     console.log(i)
