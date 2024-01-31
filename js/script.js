a91939dd8a86243ed0937d61446534b9083445c3
roberto-palanga <r.palanga94@gmail.com>
2024-01-31 16:22:16 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const grigliaElement = document.querySelector("#griglia")
+
+for (let i = 0 ; i <= 100 ; i++) {
+    
+    grigliaElement.innerHTML += `<div>Cassone culo</div>`
+    console.log(grigliaElement)
+}
